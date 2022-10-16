07c6373f466dc08b5a1aa7b19e26e44c8ad3a984
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-10-16 15:38:29 -0500

    Create inmethod to centralize ingredients data and set it as a property

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,22 +1,58 @@
-import ingredientsData from '../data/ingredients';
+import ingredientsData from '../data/ingredients'
+import Ingredient from './Ingredient'
 
 class Recipe {
   constructor(recipeData) {
-    this.id = recipeData.id;
-    this.image = recipeData.image;
-    this.ingredients = recipeData.ingredients;
-    this.instructions = recipeData.instructions;
-    this.name = recipeData.name;
-    this.tags = recipeData.tags;
+    this.id = recipeData.id
+    this.image = recipeData.image
+    this.recipeIngredients = recipeData.ingredients
+    this.instructions = recipeData.instructions
+    this.name = recipeData.name
+    this.tags = recipeData.tags
+    this.ingredients = this.getAllIngredientsData()
+  }
+
+  getAllIngredientsData() {
+    // For each ingredient contained in this.ingredients, we want to:
+    // 1. Store the ID, amount, and units data of that ingredient in this.ingredientsRepo
+    // 2. Find the object with a matching ID in the ingredientsData array
+    // 3. Store the name and estimated cost in ingredientsRepo
+    return this.recipeIngredients.map((ingredient) => {
+      const id = ingredient.id
+      const amount = ingredient.quantity.amount
+      const units = ingredient.quantity.unit
+      const matchedIngredient = ingredientsData.find(
+        (ingredient) => ingredient.id === id
+      )
+
+      return new Ingredient(
+        id,
+        matchedIngredient.name,
+        matchedIngredient.estimatedCostInCents,
+        units,
+        amount
+      )
+    })
+
+    // const ids = this.ingredients.map((ingredient) => ingredient.id)
+    // return ingredientsData.filter((ingredient) => ids.includes(ingredient.id))
   }
 
   findIngredeintNames() {
-    const ids = this.ingredients.map(ingredient => ingredient.id)
+    const matchedIngredients = this.getAllIngredientsData()
+    return matchedIngredients.map((ingredient) => ingredient.name)
+  }
 
-    const matchedIngredients = ingredientsData.filter(ingredient => ids.includes(ingredient.id))
+  totalCost() {
+    const matchedIngredients = this.getAllIngredientsData()
+    const costs = matchedIngredients.map(
+      (ingredient) => ingredient.estimatedCostInCents
+    )
+    // this.ingredients.reduce((accumulator, value) => {
 
-     return matchedIngredients.map(ingredient => ingredient.name)
+    // })
+    // amounts.find(ingredient => ingredient.id)
   }
 }
 
-export default Recipe;
+export default Recipe
