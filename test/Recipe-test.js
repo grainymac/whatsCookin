dfcaffe0a46f0af6bfa0db9c8e5cd3eb035810f0
James Wasmer <jameswasmer@gmail.com>
2022-10-16 14:08:04 -0400

    Test findName() method

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -28,62 +28,6 @@ describe('Recipe', () => {
             "amount": 1,
             "unit": "large"
           }
-        },
-        {
-          "id": 19335,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "c"
-          }
-        },
-        {
-          "id": 19206,
-          "quantity": {
-            "amount": 3,
-            "unit": "Tbsp"
-          }
-        },
-        {
-          "id": 19334,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "c"
-          }
-        },
-        {
-          "id": 2047,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "tsp"
-          }
-        },
-        {
-          "id": 1012047,
-          "quantity": {
-            "amount": 24,
-            "unit": "servings"
-          }
-        },
-        {
-          "id": 10019903,
-          "quantity": {
-            "amount": 2,
-            "unit": "c"
-          }
-        },
-        {
-          "id": 1145,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "c"
-          }
-        },
-        {
-          "id": 2050,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "tsp"
-          }
         }
       ],
       "instructions": [
@@ -162,62 +106,6 @@ describe('Recipe', () => {
           "amount": 1,
           "unit": "large"
         }
-      },
-      {
-        "id": 19335,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "c"
-        }
-      },
-      {
-        "id": 19206,
-        "quantity": {
-          "amount": 3,
-          "unit": "Tbsp"
-        }
-      },
-      {
-        "id": 19334,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "c"
-        }
-      },
-      {
-        "id": 2047,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "tsp"
-        }
-      },
-      {
-        "id": 1012047,
-        "quantity": {
-          "amount": 24,
-          "unit": "servings"
-        }
-      },
-      {
-        "id": 10019903,
-        "quantity": {
-          "amount": 2,
-          "unit": "c"
-        }
-      },
-      {
-        "id": 1145,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "c"
-        }
-      },
-      {
-        "id": 2050,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "tsp"
-        }
       }
     ]);
   })
@@ -264,5 +152,9 @@ describe('Recipe', () => {
       "antipasto",
       "hor d'oeuvre"
     ]);
+  })
+
+  it('should have a method that returns an ingredient name', () => {
+    expect(recipe.findName(recipe.ingredients)).to.deep.equal(["wheat flour", "bicarbonate of soda", "eggs"])
   })
 })
