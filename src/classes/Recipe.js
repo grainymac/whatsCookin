7b4e1f0d8a6e241a195231e5ddef1929e98f1e40
James Wasmer <jameswasmer@gmail.com>
2022-10-16 13:44:20 -0400

    Build Recipe constructor and test that it accepts a parameter

---
+++ src/classes/Recipe.js
@@ -0,0 +1,11 @@
+class Recipe {
+  constructor(recipeData) {
+    this.id = recipeData.id;
+    this.image = recipeData.image;
+    this.ingredients = recipeData.ingredients;
+    this.name = recipeData.name;
+    this.tags = recipeData.tags;
+  }
+}
+
+module.exports = Recipe;
