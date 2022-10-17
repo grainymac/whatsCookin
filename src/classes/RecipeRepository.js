c0696bf8dbc91a10f05ea5757a23b15221788f1f
James Wasmer <jameswasmer@gmail.com>
2022-10-17 17:09:15 -0400

    Fix makeRecipeInstances by passing currentRecipe as Recipe parameter

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -4,25 +4,11 @@ class RecipeRepository {
   constructor(allRecipesData) {
     this.allRecipes = this.makeRecipeInstances(allRecipesData);
     this.userSelectedTag = '';
-    // One class to get you started!
   }
 
   makeRecipeInstances(recipeList) {
     return recipeList.map((currentRecipe) => {
-      const id = currentRecipe.id;
-      const image = currentRecipe.image;
-      const recipeIngredients = currentRecipe.recipeIngredients
-      const instructions = currentRecipe.instructions;
-      const name = currentRecipe.name;
-      const tags = currentRecipe.tags;
-      return new Recipe(
-        id,
-        image,
-        recipeIngredients,
-        instructions,
-        name,
-        tags
-      )
+      return new Recipe(currentRecipe)
     })
   }
 }
