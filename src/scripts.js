61f2d8d92e7ab1a2a1bd872ead9593cfd9262873
James Wasmer <jameswasmer@gmail.com>
2022-10-21 19:02:10 -0400

    Create a new User class instance from a randomly selected user from usersData

--- src/scripts.js
+++ src/scripts.js
@@ -5,8 +5,13 @@ import './images/turing-logo.png';
 import Recipe from './classes/Recipe';
 import RecipeRepository from './classes/RecipeRepository';
 import recipeData from './data/recipes';
+import User from './classes/User';
+import usersData from './data/users';
 // ------------------- GLOBAL VARIABLES
 const recipeRepo = new RecipeRepository(recipeData);
+const userData = usersData[Math.floor(Math.random() * usersData.length)]
+console.log(userData)
+const user = new User(userData)
 let tag;
 let tagList = [];
 
