7302c976afea564c8dd5bdabf869a990cb77e0fc
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-10-21 19:20:02 -0500

    Pass total cost test and return instructions

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -167,14 +167,14 @@ describe('Recipe', () => {
   })
 
   it('should have a method that returns the total cost of ingredients in cents', () => {
-    expect(recipe.totalCost()).to.equal(976)
+    expect(recipe.totalCost()).to.equal('9.76')
   })
 
   it('should return the instructions', () => {
     expect(recipe.getInstructions()).to.deep.equal([
-      'In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.',
-      'Add egg and vanilla and mix until combined.',
-      'Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.',
+      '1: In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.',
+      '2: Add egg and vanilla and mix until combined.',
+      '3: Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.',
     ])
   })
 })
