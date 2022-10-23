73d2ab121ee6612c19fcc9a4148fc82a6d9c879c
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-10-23 01:45:30 -0500

    Refactor recipe and recipe repo to pass in ingredientsData as an argument

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,25 +1,24 @@
-import ingredientsData from '../data/ingredients'
-import Ingredient from './Ingredient'
+import Ingredient from './Ingredient';
 
 class Recipe {
-  constructor(recipeData) {
-    this.id = recipeData.id
-    this.image = recipeData.image
-    this.recipeIngredients = recipeData.ingredients
-    this.instructions = recipeData.instructions
-    this.name = recipeData.name
-    this.tags = recipeData.tags
-    this.ingredients = this.getAllIngredientsData()
+  constructor(recipeData, ingredientsData) {
+    this.id = recipeData.id;
+    this.image = recipeData.image;
+    this.recipeIngredients = recipeData.ingredients;
+    this.instructions = recipeData.instructions;
+    this.name = recipeData.name;
+    this.tags = recipeData.tags;
+    this.ingredients = this.getAllIngredientsData(ingredientsData);
   }
 
-  getAllIngredientsData() {
+  getAllIngredientsData(ingredientsData) {
     return this.recipeIngredients.map((ingredient) => {
-      const id = ingredient.id
-      const amount = ingredient.quantity.amount
-      const unit = ingredient.quantity.unit
+      const id = ingredient.id;
+      const amount = ingredient.quantity.amount;
+      const unit = ingredient.quantity.unit;
       const matchedIngredient = ingredientsData.find(
         (ingredient) => ingredient.id === id
-      )
+      );
 
       return new Ingredient(
         id,
@@ -27,24 +26,27 @@ class Recipe {
         matchedIngredient.estimatedCostInCents,
         amount,
         unit
-      )
-    })
+      );
+    });
   }
 
   findIngredientNames() {
-    return this.ingredients.map((ingredient) => ingredient.name)
+    return this.ingredients.map((ingredient) => ingredient.name);
   }
 
   totalCost() {
-    const cost = this.ingredients.reduce((accumulator, value) => {
-      return accumulator += value.estimatedCostInCents * value.amount
-    }, 0) / 100
-    return cost.toFixed(2)
+    const cost =
+      this.ingredients.reduce((accumulator, value) => {
+        return (accumulator += value.estimatedCostInCents * value.amount);
+      }, 0) / 100;
+    return cost.toFixed(2);
   }
 
   getInstructions() {
-    return this.instructions.map(recipeStep => `${recipeStep.number}: ${recipeStep.instruction}`)
+    return this.instructions.map(
+      (recipeStep) => `${recipeStep.number}: ${recipeStep.instruction}`
+    );
   }
 }
 
-export default Recipe
+export default Recipe;
