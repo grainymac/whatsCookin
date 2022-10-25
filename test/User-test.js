bc0882637bf21be273db336f9da5b73494a1fed6
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-10-24 20:49:53 -0500

    Update tests to have ingredients data and pass all tests

--- test/User-test.js
+++ test/User-test.js
@@ -6,7 +6,7 @@ import Recipe from '../src/classes/Recipe';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
 describe('User', () => {
-  let user, recipeData, recipe, anotherRecipe;
+  let user, recipe, anotherRecipe;
 
   beforeEach(() => {
     user = new User({
@@ -14,78 +14,111 @@ describe('User', () => {
       id: 1,
     });
 
-    recipe = new Recipe({
-      id: 231951,
-      image: 'https://spoonacular.com/recipeImages/231951-556x370.jpg',
-      ingredients: [
+    recipe = new Recipe(
+      {
+        id: 231951,
+        image: 'https://spoonacular.com/recipeImages/231951-556x370.jpg',
+        ingredients: [
+          {
+            id: 20027,
+            quantity: {
+              amount: 2,
+              unit: 'tablespoons',
+            },
+          },
+          {
+            id: 10019334,
+            quantity: {
+              amount: 0.5,
+              unit: 'cup',
+            },
+          },
+        ],
+        instructions: [
+          {
+            instruction:
+              'Whisk together brown sugar, cornstarch, and 1/4 teaspoon salt in a heavy medium saucepan, then whisk in milk and cream. Bring to a boil over medium heat, whisking frequently, then boil, whisking, 1 minute.',
+            number: 1,
+          },
+        ],
+        name: 'Butterscotch Pudding',
+        tags: ['side dish'],
+      },
+      [
         {
           id: 20027,
-          quantity: {
-            amount: 2,
-            unit: 'tablespoons',
-          },
+          name: 'corn starch',
+          estimatedCostInCents: 236,
         },
         {
           id: 10019334,
+          name: 'dark brown sugar',
+          estimatedCostInCents: 501,
+        },
+      ]
+    );
+  });
+
+  anotherRecipe = new Recipe(
+    {
+      id: 999044,
+      image: 'https://spoonacular.com/recipeImages/999044-556x370.jpg',
+      ingredients: [
+        {
+          id: 9037,
           quantity: {
-            amount: 0.5,
-            unit: 'cup',
+            amount: 1,
+            unit: '',
+          },
+        },
+        {
+          id: 16057,
+          quantity: {
+            amount: 1.5,
+            unit: 'cups',
+          },
+        },
+        {
+          id: 11297,
+          quantity: {
+            amount: 2,
+            unit: 'tablespoons',
           },
         },
       ],
       instructions: [
         {
           instruction:
-            'Whisk together brown sugar, cornstarch, and 1/4 teaspoon salt in a heavy medium saucepan, then whisk in milk and cream. Bring to a boil over medium heat, whisking frequently, then boil, whisking, 1 minute.',
+            'Working in a couple of batches, roughly chop the chickpeas and place in a bowl.',
           number: 1,
         },
+        {
+          instruction:
+            'Add the minced herbs and scallions to the chickpeas and give a quick toss.',
+          number: 2,
+        },
       ],
-      name: 'Butterscotch Pudding',
-      tags: ['side dish'],
-    });
-  });
-
-  anotherRecipe = new Recipe({
-    id: 999044,
-    image: 'https://spoonacular.com/recipeImages/999044-556x370.jpg',
-    ingredients: [
+      name: 'Avocado Chickpea Salad',
+      tags: ['salad'],
+    },
+    [
       {
         id: 9037,
-        quantity: {
-          amount: 1,
-          unit: '',
-        },
+        name: 'haas avocados',
+        estimatedCostInCents: 275,
       },
       {
         id: 16057,
-        quantity: {
-          amount: 1.5,
-          unit: 'cups',
-        },
+        name: 'garbanzos',
+        estimatedCostInCents: 85,
       },
       {
         id: 11297,
-        quantity: {
-          amount: 2,
-          unit: 'tablespoons',
-        },
+        name: 'flat leaf parsley leaves',
+        estimatedCostInCents: 1030,
       },
-    ],
-    instructions: [
-      {
-        instruction:
-          'Working in a couple of batches, roughly chop the chickpeas and place in a bowl.',
-        number: 1,
-      },
-      {
-        instruction:
-          'Add the minced herbs and scallions to the chickpeas and give a quick toss.',
-        number: 2,
-      },
-    ],
-    name: 'Avocado Chickpea Salad',
-    tags: ['salad'],
-  });
+    ]
+  );
 
   it('should be a function', () => {
     expect(User).to.be.a('function');
