7623ffcbe7512ebb2fcc9843ae3959567cd419f6
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-11-02 17:15:17 -0500

    Update pantry methods to use one method

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,80 +1,54 @@
-import Recipe from './Recipe';
-import RecipeRepository from './RecipeRepository';
-
+import Recipe from './Recipe'
+import RecipeRepository from './RecipeRepository'
 
 class User {
-    constructor(userData = {}) {
-        this.id = userData.id
-        this.name = userData.name
-        this.pantry = userData.pantry
-        this.canCook = [];
-        this.cannotCook = [];
-        this.favoriteRecipeRepo = new RecipeRepository()
-    }
+  constructor(userData = {}) {
+    this.id = userData.id
+    this.name = userData.name
+    this.pantry = userData.pantry
+    this.favoriteRecipeRepo = new RecipeRepository()
+  }
 
-    addFavoriteRecipe = (recipe) => {
-        return this.favoriteRecipeRepo.addRecipe(recipe)
-    }
+  addFavoriteRecipe = (recipe) => {
+    return this.favoriteRecipeRepo.addRecipe(recipe)
+  }
 
-    removeFavoriteRecipe = (recipe) => {
-        return this.favoriteRecipeRepo.removeRecipe(recipe)
-    }
+  removeFavoriteRecipe = (recipe) => {
+    return this.favoriteRecipeRepo.removeRecipe(recipe)
+  }
 
-    filterFavoriteRecipesByTag(tag) {
-        return this.favoriteRecipeRepo.filterByTag(tag)
-    }
-    
-    searchFavoriteRecipesByName(userSearch) {
-        return this.favoriteRecipeRepo.searchByName(userSearch)
-    }
+  filterFavoriteRecipesByTag(tag) {
+    return this.favoriteRecipeRepo.filterByTag(tag)
+  }
 
-    findIngredientsInPantry(recipe) {
-        const pantryIDs = this.pantry.map((pantryIngredient) => {
-            return pantryIngredient.ingredient
+  searchFavoriteRecipesByName(userSearch) {
+    return this.favoriteRecipeRepo.searchByName(userSearch)
+  }
+
+  getMissingIngredientsForRecipe(recipe) {
+    const missingIngredients = []
+
+    recipe.ingredients.forEach((recipeIngredient) => {
+      const foundIngredient = this.pantry.find((pantryIngredient) => {
+        return recipeIngredient.id === pantryIngredient.ingredient
+      })
+      if (!foundIngredient) {
+        missingIngredients.push({
+          id: recipeIngredient.id,
+          name: recipeIngredient.name,
+          amount: recipeIngredient.amount,
         })
-        const notInPantry = recipe.ingredients.filter((recipeIngredient) => {
-            return !pantryIDs.includes(recipeIngredient.id)
+      } else if (foundIngredient.amount < recipeIngredient.amount) {
+        missingIngredients.push({
+          id: recipeIngredient.id,
+          name: recipeIngredient.name,
+          amount: recipeIngredient.amount - foundIngredient.amount,
         })
-        if(!notInPantry) {
-            return this.checkIngredientAmounts(recipe)
-        } else {
-            this.determineMissingIngredients(recipe);
-            return false
-        }
-    }
+      }
+    })
 
-    checkIngredientAmounts(recipe) {
-        const isEnough = this.pantry.every((pantryIngredient) => {
-            const foundIngredient = recipe.ingredients.find((ingredient) => {
-                return ingredient.id === pantryIngredient.ingredient
-            })
-            return foundIngredient.amount <= pantry.amount
-        })
-        if(!isEnough) {
-            this.determineMissingIngredients(recipe);
-        }
-        return isEnough
-    }
-
-    determineMissingIngredients(recipe) {
-        const neededIngredients = recipe.ingredients.reduce((acc, recipeIngredient) => {
-            const foundPantryIngredient = this.pantry.find((pantryIngredient) => {
-                return pantryIngredient.ingredient === recipeIngredient.id
-            })
-            if(foundPantryIngredient) {
-                console.log(foundPantryIngredient)
-                if(foundPantryIngredient.amount <= recipeIngredient.amount) {
-                    acc.push({name: `${recipeIngredient.name}`, needed: `${foundPantryIngredient.amount - recipeIngredient.amount}`)
-                }
-            } else if(!foundPantryIngredient) {
-                acc.push(`${recipeIngredient.amount} ${recipeIngredient.name} `)
-            }
-        return acc;
-        }, [])
-        return neededIngredients
-    }
-
- }
-
+    return missingIngredients
+  }
+}
 
 export default User
