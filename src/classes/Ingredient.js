9de5bedfdcf966c56481d830eb957dec90277a7d
James Wasmer <jameswasmer@gmail.com>
2022-11-03 19:58:58 -0400

    Refactor static method to accept two parameters, ingredientSource and ingredientsData

--- src/classes/Ingredient.js
+++ src/classes/Ingredient.js
@@ -3,24 +3,21 @@ class Ingredient {
     this.id = data.id
     this.name = data.name
     this.cost = data.cost
-    this.amount = data?.amount
+    this.amount = data.amount
     this.unit = data?.unit
   }
 
-  static fromIngredientData(id, ingredientsData, recipeData = null) {
-    const fromIngredients = ingredientsData.find(ingredient => id === ingredient.id)
-    let fromRecipe;
+  static fromIngredientData(ingredientSource, ingredientsData) {
+    const fromIngredients = ingredientsData.find(ingredient => ingredient.id === Object.values(ingredientSource)[0])
     
     let allIngredientData = {}
 
-    allIngredientData.id = id
+    allIngredientData.id = fromIngredients.id
     allIngredientData.name = fromIngredients.name
     allIngredientData.cost = fromIngredients.estimatedCostInCents
-    if (recipeData) {
-      fromRecipe = recipeData.ingredients.find(ingredient => id === ingredient.id)
-
-      allIngredientData.amount = fromRecipe.quantity.amount
-      allIngredientData.unit = fromRecipe.quantity.unit
+    allIngredientData.amount = ingredientSource.amount || ingredientSource.quantity.amount
+    if (ingredientSource.quantity) {
+      allIngredientData.unit = ingredientSource.quantity.unit
     }
 
     return new Ingredient(allIngredientData)
